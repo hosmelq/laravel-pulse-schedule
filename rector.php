902993475c468a2445000e3eea3a1f196c9453f6
Hosmel Quintana <hosmelq@gmail.com>
2024-02-15 10:47:01 -0600

    update dependencies

--- rector.php
+++ rector.php
@@ -2,30 +2,20 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 
-return static function (RectorConfig $config): void {
-    $config->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__.'/src',
-    ]);
-
-    $config->rules([
-        InlineConstructorDefaultToPropertyRector::class,
-    ]);
-
-    $config->sets([
-        LevelSetList::UP_TO_PHP_81,
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::EARLY_RETURN,
-        SetList::TYPE_DECLARATION,
-    ]);
-
-    $config->skip([
+    ])
+    ->withPhpSets()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        earlyReturn: true,
+    )
+    ->withSkip([
         ClosureToArrowFunctionRector::class,
     ]);
-};
