4e57645bbfffbc3a9845796fa5af394dfffef21b
Igor Finagin <Igor@Finag.in>
2024-02-17 10:41:54 +0700

    Laravel 11 support

--- src/ScheduleServiceProvider.php
+++ src/ScheduleServiceProvider.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace HosmelQ\Laravel\Pulse\Schedule;
 
 use HosmelQ\Laravel\Pulse\Schedule\Livewire\Schedule;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 use Livewire\LivewireManager;
 
-class ScheduleServiceProvider extends ServiceProvider
+class ScheduleServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     public function boot(): void
     {
@@ -17,5 +18,13 @@ class ScheduleServiceProvider extends ServiceProvider
         $this->callAfterResolving('livewire', function (LivewireManager $livewire): void {
             $livewire->component('pulse.schedule', Schedule::class);
         });
+    }
+
+    /**
+     * @return array<class-string>
+     */
+    public function provides()
+    {
+        return [\Laravel\Pulse\Pulse::class];
     }
 }
